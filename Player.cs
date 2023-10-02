e579fd8ca04294a498af6c4f31a427e326e14fab
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-10-02 16:48:13 -0700

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refresher
+{
+    public class Player
+    {
+        //Fields
+        string _name;
+        int _number;
+
+        //constructor
+        public Player(string name, int number)
+        {
+            _name = name;
+            _number = number;
+        }
+
+        //properties
+        public string Name { get => _name; set => _name = value; }
+        public int Number { get => _number; set => _number = value; }
+
+        //methods
+        public override string ToString()
+        {
+            return $"Name - {_name} Number - {_number}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+
+namespace Refresher
+{
+    internal class Program
+    {
+        //creating empty list of players
+        static List<Player> players = new List<Player>();
+        static void Main(string[] args)
+        {
+            //create new players
+            Player player1 = new Player("Shiva", 45);
+            Player player2 = new Player("Tyler", 30);
+            Player player3 = new Player("Fluffy", 36);
+            Player player4 = new Player("Odin", 100);
+            Player player5 = new Player("Tinsil", 85);
+
+            //add players to list
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+            players.Add(player4);
+            players.Add(player5);
+
+            //display list
+            for (int i = 0; i < players.Count; i++)
+            {
+                Console.WriteLine($"{players[i]}");
+            }
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (players[i].Number % 2 != 0)
+                {
+                    Console.WriteLine($"{players[i]}");
+                }
+            }
+        }
+    }
+}
